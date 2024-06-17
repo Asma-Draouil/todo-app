ed6dfc94a4bc6e550e12a088b008417bb7c77517
Asma Draouil <asmadraouil2@gmail.com>
2024-06-17 23:04:41 +0100

    services impl

--- src/main/java/com/project/todo/services/impl/TodoServiceImpl.java
+++ src/main/java/com/project/todo/services/impl/TodoServiceImpl.java
@@ -1,5 +1,58 @@
 package com.project.todo.services.impl;
 
-public class TodoServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.project.todo.dto.TodoDto;
+import com.project.todo.repositories.TodoRepository;
+import com.project.todo.services.TodoService;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
+	
+	private final TodoRepository todoRepository;
+	
+
+	@Override
+	public TodoDto save(TodoDto todo) {
+		
+		return TodoDto.fromEntity(todoRepository.save(TodoDto.toEntity(todo)));
+	}
+
+	@Override
+	public List<TodoDto> findAll() {
+		
+		return todoRepository.findAll().stream()
+				.map(TodoDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public TodoDto findById(long id) {
+		
+		return todoRepository.findById(id)
+				.map(TodoDto::fromEntity)
+				.orElseThrow(()-> new EntityNotFoundException("No Todo is found with ID = " +id));
+	}
+
+	@Override
+	public void delete(long id) {
+	todoRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public List<TodoDto> findByCategorie(long categorieId) {
+	
+		return todoRepository.findByCategorieId(categorieId).stream()
+				.map(TodoDto::fromEntity)
+				.collect(Collectors.toList());
+	}
 
 }
--- src/main/java/com/project/todo/services/impl/UserServiceImpl.java
+++ src/main/java/com/project/todo/services/impl/UserServiceImpl.java
@@ -1,5 +1,57 @@
 package com.project.todo.services.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+import com.project.todo.dto.UserDto;
+import com.project.todo.repositories.UserRepository;
+import com.project.todo.services.UserService;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+	
+	private final UserRepository userRepository;
+	
+	@Override
+	public UserDto save(UserDto user) {
+	
+		return UserDto.fromEntity(userRepository.save(UserDto.toEntity(user)));
+	}
+
+	@Override
+	public List<UserDto> findAll() {
+		
+		return userRepository.findAll().stream()
+				.map(UserDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public UserDto findById(long id) {
+
+		return userRepository.findById(id).map(UserDto::fromEntity)
+				.orElseThrow(()-> new EntityNotFoundException("No user is found with ID = " +id));
+	}
+
+	@Override
+	public void delete(long id) {
+		userRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public UserDto login(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
 }
